abc584d86129789f86a5db0c32542053d977743c
Gulshan06 <gullukr2132@gmail.com>
2021-07-21 12:07:52 +0530

    python list tuple dic

---
+++ listmembership.py
@@ -0,0 +1,10 @@
+students = [2,3,4,6,8,9,"ram","shyam","gulshan","arun"]
+li = [22,444,32,65,1]
+name = 11
+if(name in students):
+    print("he is present")
+else:
+    print("he is absent")
+print(max(li))
+print(min(li))
+print(tuple(li))
